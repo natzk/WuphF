61912586c5630af61e4cf70288bebfa1aea35e91
natzk <shkalim.ben@gmail.com>
2022-05-27 14:04:24 +0300

    Uploading the project after balagan

---
+++ app/src/main/java/com/example/wuphf/FaqsFragment.kt
@@ -0,0 +1,48 @@
+package com.example.wuphf
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.wuphf.databinding.FragmentFaqsBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [FaqsFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class FaqsFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+
+//    private var _binding: FragmentFaqsBinding? = null
+//    private val binding get() = _binding!!
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+//        _binding = FragmentFaqsBinding.inflate(inflater, container, false)
+//        binding.faqs.LayoutManager = LinearLayoutManager(requireActivity())
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_faqs, container, false)
+    }
+
+
+}
